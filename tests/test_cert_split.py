6aa00f5159273b1fafb31594165b9ddd69777b9e
Bas van Driel <contact@basvandriel.nl>
2024-11-22 10:08:22 +0100

    Cleanup test

--- tests/test_cert_split.py
+++ tests/test_cert_split.py
@@ -3,6 +3,6 @@ TEST_CERT = "-----BEGIN CERTIFICATE-----\ntestcert1\n-----END CERTIFICATE-----\n
 
 def test_cert_split():
     # Split on the second certificate ending so the first one is the user certificate
-    x = TEST_CERT.split("\n-----BEGIN CERTIFICATE-----\n")
+    cert1, _ = TEST_CERT.split("\n-----BEGIN CERTIFICATE-----\n")
 
-    print(x)
+    assert cert1 == "-----BEGIN CERTIFICATE-----\ntestcert1\n-----END CERTIFICATE-----"
