6aca53311b8033f8652ddedc5acac478c38c2b7b
Bas van Driel <contact@basvandriel.nl>
2024-12-05 11:38:16 +0100

    First step: refactor + complete emit

--- app/page/selectkey.py
+++ app/page/selectkey.py
@@ -1,8 +1,8 @@
+from typing import Any, Optional
 from PyQt6.QtWidgets import (
     QWizardPage,
     QVBoxLayout,
     QListWidget,
-    QWizard,
     QListWidgetItem,
 )
 
@@ -10,51 +10,88 @@ from .yubikeyitem import YubiKeyItemWidget
 
 
 class SelectYubiKeyPage(QWizardPage):
+    selectedYubiKey: Optional[Any]
+    yubikeyListWidget: QListWidget
+
+    def _get_yubikeys(self):
+        keys = []
+        for slot in self.pkcs.pkcs11.getSlotList():
+            try:
+                info = self.pkcs.pkcs11.getTokenInfo(slot)
+                keys += [(info.model, info.serialNumber, info.label, slot)]
+            except Exception:
+                pass
+
+        return keys
+
+    def _build_yubikey_list_widget(self, yubikeys: list[Any]) -> QListWidget:
+        widget = QListWidget()
+        # widget.setSelectionMode(QListWidget.SelectionMode.NoSelection)
+
+        for name, serial, available, slot in yubikeys:
+            itemWidget = YubiKeyItemWidget(name, serial, available, slot)
+            item = QListWidgetItem()
+            item.setSizeHint(itemWidget.sizeHint())
+
+            widget.addItem(item)
+            widget.setItemWidget(item, itemWidget)
+
+        return widget
+
     def __init__(self, mypkcs, parent=None):
         super().__init__(parent)
         self.setTitle("Selecteer de te gebruiken yubikey")
 
         layout = QVBoxLayout(self)
 
-        self.yubikeyListWidget = QListWidget()
-
         self.selectedYubiKey = None
-        self.yubikeyListWidget.currentItemChanged.connect(self.yubiKeySelected)
         self.pkcs = mypkcs
 
-        # Sample YubiKeys
-        yubikeys = []
-        for slot in self.pkcs.pkcs11.getSlotList():
-            try:
-                info = self.pkcs.pkcs11.getTokenInfo(slot)
-                yubikeys += [(info.model, info.serialNumber, info.label, slot)]
-            except Exception:
-                pass
+        yubikeys = self._get_yubikeys()
 
-        for name, serial, available, slot in yubikeys:
-            itemWidget = YubiKeyItemWidget(name, serial, available, slot)
-            item = QListWidgetItem()
-            self.yubikeyListWidget.addItem(item)
-            self.yubikeyListWidget.setItemWidget(item, itemWidget)
-            item.setSizeHint(itemWidget.sizeHint())
+        yubikey_list_widget = self._build_yubikey_list_widget(yubikeys)
+        layout.addWidget(yubikey_list_widget)
 
-        layout.addWidget(self.yubikeyListWidget)
+        # Save it for later reference (can this be done better?)
+        self.yubikeyListWidget = yubikey_list_widget
 
-        self.yubikeyListWidget.currentItemChanged.connect(self.enableNextButton)
+        # yubikey_list_widget.currentItemChanged.connect(self.on_yubikey_row_change)
 
-    def yubiKeySelected(self, current, _previous):
-        if current is not None:
+        # # yubikey_list_widget.currentRowChanged.connect(self.yubiKeySelected)
+        # yubikey_list_widget.itemClicked.connect(self.on_yubikey_row_click)
+
+    def on_yubikey_row_click(self, item: QListWidgetItem):
+        # TODO check here for current selection??? otherwise do nothing
+        # TODO de-select the yubikey on page exit
+        if item is not None:
             # Assuming the YubiKeyItemWidget has a method or property to get the YubiKey details
-            self.selectedYubiKey = self.yubikeyListWidget.itemWidget(current).getYubiKeyDetails()
-            self.wizard().button(QWizard.WizardButton.NextButton).setEnabled(True)
+            self.selectedYubiKey = self.yubikeyListWidget.itemWidget(item).getYubiKeyDetails()
+
+            # The next button does not have to be enabled manually, just trigger the completion signal.
+            # This will re-check the isCompleted function
+            # https://doc.qt.io/qt-6/qwizardpage.html#completeChanged
+            self.completeChanged.emit()
+
+    def on_yubikey_row_change(self, current: QListWidgetItem, previous: Optional[QListWidgetItem]):
+        # TODO check here for current selection??? otherwise select other one
+        item_in_list = self.yubikeyListWidget.itemWidget(current)
+
+        print(1)
+
+    def isComplete(self) -> bool:
+        return self.selectedYubiKey is not None
 
     def nextId(self):
         # Store the selected YubiKey information in the wizard
         self.wizard().setProperty("selectedYubiKey", self.selectedYubiKey)
         return super().nextId()
 
-    def initializePage(self):
-        self.wizard().button(QWizard.WizardButton.NextButton).setEnabled(False)
+    def initializePage(self) -> None:
+        super().initializePage()
 
-    def enableNextButton(self):
-        self.wizard().button(QWizard.WizardButton.NextButton).setEnabled(True)
+        self.yubikeyListWidget.clearSelection()
+
+        self.yubikeyListWidget.currentItemChanged.connect(self.on_yubikey_row_change)
+
+        # yubikey_list_widget.currentRowChanged.connect(self.yubiKeySelected)
+        self.yubikeyListWidget.itemClicked.connect(self.on_yubikey_row_click)
