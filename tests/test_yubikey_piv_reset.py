1a509c80d746c021ae378f5e45cd294c60aa4f82
Bas van Driel <contact@basvandriel.nl>
2024-12-12 11:35:25 +0100

    work on test

--- tests/test_yubikey_piv_reset.py
+++ tests/test_yubikey_piv_reset.py
@@ -1,9 +1,13 @@
+from unittest.mock import MagicMock, patch
 import pytest
 from app.yubikey_details import YubikeyDetails
 from app.yubikey_piv_resetter import YubiKeyPIVResetter
 
 
-def test_yubikey_reset_not_found():
+@patch("app.yubikey_piv_resetter.subprocess.run")
+def test_yubikey_reset_not_found(run_mock: MagicMock):
+    run_mock.return_value.stdout.decode.return_value = "ERROR: Failed connecting to a YubiKey with serial: 1"
+
     details = YubikeyDetails("1", "1", "123")
 
     r = YubiKeyPIVResetter()
