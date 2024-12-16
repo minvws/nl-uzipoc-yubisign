a68c7ab45396a2c6f01253d3a34771767f95b556
Bas van Driel <contact@basvandriel.nl>
2024-12-16 14:51:13 +0100

    work

--- app/wizard.py
+++ app/wizard.py
@@ -48,7 +48,7 @@ class MainWindow(QWizard):
 
         # When the wizard has finished, close the application
         self.finished.connect(QApplication.instance().quit)
-        self.setWindowFlags(Qt.WindowType.MSWindowsFixedSizeDialogHint)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint)
 
 
 if __name__ == "__main__":
